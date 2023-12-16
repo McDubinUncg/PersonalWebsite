2d4212aaab1d0b0d6f37dc4dc1b1291c9805ad20
John Abdellmelk <jnabdell@uncg.edu>
2023-12-16 18:52:39 -0500

    made some more foundational setups

--- app.js
+++ app.js
@@ -5,10 +5,22 @@ const app = express();
 
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
+const aboutRouter = require('./routes/about');
+app.use('/about', aboutRouter);
+
+const contactRouter = require('./routes/contact');
+app.use('/contact', contactRouter);
+
+const blogRouter = require('./routes/blog');
+app.use('/blog', blogRouter);
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
